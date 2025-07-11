2bbf55088a0fceeeed7c8b00fbb0a27f7595b9b8
NawaleMahesh23 <maheshnawale8421@gmail.com>
2025-07-11 13:33:29 +0530

    todays lern js for  operators if else datatype

--- js/script.js
+++ js/script.js
@@ -1,3 +1,73 @@
 function external(){
     alert("external js ex")
 }
+// console.log("hello world");
+// let a=5;
+// let b=7;
+// console.log("a+b=",a+b);
+// console.log("a-b",a-b);
+
+// let a=6;
+// let b=6;
+// console.log("a=",a, "& b=",b);
+// a++;
+// b--;
+
+// console.log("a",a);
+// console.log("b",b);
+
+// let a=5;
+// let b=2;
+
+// a+=4;
+// console.log("a =",a);
+// a-=4;
+// console.log("a =",a);
+
+// a*=4;
+// console.log("a =",a);
+
+// let age=18;
+// if(age>=18){
+//     console.log("we can vote");
+// }
+// if(age<18){
+//     console.log("we CANNOT vote");
+// }
+// let age=25;
+
+// age>=18 ? console.log("adult") : console.log("not adult");
+//  let name=prompt("hello!");
+//  console.log(name);
+
+// let num=prompt("Enter a num");
+// if(num %5===0){
+//     console.log( num," is a multiple 5")
+// }else{
+//     console.log(num,"not multiple");
+// }
+// let score=95;
+// let grade;
+
+// if(score >=80 && score <=100){
+//     grade="A";
+// }else if(score >=70 && score <=89){
+//     grade="B";
+// }else if( score >=60 && score <=69){
+//      grade="C";
+// }else if(score >=50 && score <=59){
+//      grade="D";
+// }else if(score >=0 && score <=49){
+//     grade="fail";
+// }
+// console.log("according score",grade);
+
+// let sum=0;
+// for(let i=1;i<=13;i++){
+//     sum = sum+i;
+// }
+// console.log("sum =",sum);
+let str="maheshnawale";
+for(let i of str){
+    console.log("i=",i);
+}
